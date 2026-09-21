b712fe626ce1dbeae1fd5b639c23569ff82fb8f6
Evan Prodromou <evan@prodromou.name>
2026-09-21 18:40:24 -0400

    test: application/jrd+json

---
+++ test/webfinger-jrd-test.js
@@ -0,0 +1,26 @@
+const assert = require('node:assert/strict')
+const { describe, it } = require('node:test')
+const nock = require('nock')
+const wf = require('../lib/webfinger')
+const { useNock } = require('./helpers/nock')
+
+describe('WebFinger JRD media type', function () {
+  useNock()
+
+  it('accepts an endpoint that returns only application/jrd+json', async function () {
+    const resource = 'https://localhost/profile/alice'
+    const document = {
+      subject: resource,
+      links: [{ rel: 'profile', href: resource }]
+    }
+
+    nock('https://localhost')
+      .get('/.well-known/webfinger')
+      .query({ resource })
+      .reply(200, document, { 'Content-Type': 'application/jrd+json' })
+
+    const result = await wf.webfinger(resource, null, { webfingerOnly: true })
+
+    assert.deepEqual(result, document)
+  })
+})
